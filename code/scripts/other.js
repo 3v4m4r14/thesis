d18fb410baff887619222fbe9056ef2f4b944268
Eva Maria Veitmaa <evveit@ttu.ee>
2018-04-04 14:17:13 +0300

    Added voice recognition and iris pics

--- code/scripts/other.js
+++ code/scripts/other.js
@@ -1,3 +1,14 @@
 function turnOverlayOn() {
     $('#severalFacesModal').modal('show');
 }
+
+var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+recognition.interimResults = false;
+recognition.maxAlternatives = 5;
+recognition.continuous = true;
+recognition.start();
+
+//TODO make continuous
+recognition.onspeechstart = function() {
+    console.log('You said sth!');
+};
